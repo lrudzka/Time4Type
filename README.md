716641d85000b20e3b8917af38bfb847ccd36b13
Lucyna Rudzka <lrudzka@onet.eu>
2018-06-24 23:16:19 +0200

    20180624_03

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Time4Type
 Aplikacja Time4Type do typowania wyników meczów piłkarskich
+
+Technologie: React, JavaScript, Html, Css, Sass
+
+Werjsa producyjna: http://time4type.eu
--- js/Rules.jsx
+++ js/Rules.jsx
@@ -7,12 +7,16 @@ class Rules extends React.Component{
         return(
             <Template>
                 <div className="HolyGrail-content">
-                    <ol className="rules"> <span> Zasady typowania:</span>
-                        <li> Typujemy wyniki meczów w ramach aktualnie trwającej imprezy (zakładka Home) </li>
-                        <li> Za poprawnie wytypowany wynik gracz otrzymuje 3 punkty</li>
-                        <li> Za poprawne wskazanie zwycięzcy meczu lub za poprawne wytypowanie remisu gracz otrzymuje 1 pkt</li>
-                        <li> Aby zmienić wprowadzony typ należy go usunąć z tabeli z typami i następnie wprowadzić go ponownie</li>
-                    </ol>
+                    <div className="rulesBox">
+                        <ol className="rules"> <span> Zasady typowania:</span>
+                            <li> Typujemy wyniki meczów w ramach aktualnie trwającej imprezy (zakładka Home) </li>
+                            <li> Aby wprowadzić i / lub przeglądać swoje typy należy zalogować się przez konto Google</li>
+                            <li> Za poprawne wytypowanie wyniku gracz otrzymuje 3 punkty</li>
+                            <li> Za poprawne wskazanie zwycięzcy meczu gracz otrzymuje 1 punkt</li>
+                            <li> Za poprawne wytypowanie remisu gracz otrzymuje 1 punkt</li>
+                            <li> Aby zmienić swój typ należy go najpierw usunąć z listy</li>
+                        </ol>
+                    </div>
                 </div>
             </Template>
 
